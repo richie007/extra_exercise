bfae72a1a59cbff453c200b1209897b9e5f22786
Richard <richardawale182@gmail.com>
2016-10-14 15:02:14 +0300

    express and body parser added

---
+++ index.js
@@ -0,0 +1,45 @@
+//bring in the necessary dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+// Configure the express web server framework
+var app = express();
+app.use(bodyParser.json());
+
+//define the API routes for our application
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var messages = [
+  {
+  username: "Richard",
+  message: "Hello"
+  },
+  {
+    username: "Rick",
+    message: "Hello sire"
+  }
+];
+//messages
+//get
+//posts
+
+app.get('/messages', function (req, res) {
+  res.json(messages);
+});
+
+app.post('/messages',  function (req, res) {
+  messages.push({
+    username:  req.body.username,
+    message:  req.body.message
+  });
+  res.json(messages);
+});
+
+
+//Activate the express server
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
